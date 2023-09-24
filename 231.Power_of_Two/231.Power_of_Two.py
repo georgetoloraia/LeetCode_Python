03433ef97097f72ef3e8ee6ee594e849696fad76
georgetoloraia <gtoloraia19@gmail.com>
2023-09-25 00:32:13 +0400

    problem solved

---
+++ 231.Power_of_Two/231.Power_of_Two.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+          return False
+
+        return (n & (n - 1)) == 0
